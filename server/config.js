0cfff6ce3943adfb39816a4e09b5c34ff1257d37
Sunny <sunny.yu@nih.gov>
2018-03-20 13:31:35 -0400

    Update docker

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 var config = {
     client: process.env.SQL_CLIENT || 'pg',
     connection: {
-        host : process.env.SQL_HOST || 'localhost',
+        host : process.env.SQL_HOST || 'db',
         user : process.env.SQL_USER || 'postgres',
         password : process.env.SQL_PW || 'postgres',
         database : process.env.SQL_DB || 'postgres'
