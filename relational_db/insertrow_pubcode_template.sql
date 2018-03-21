cd687eed89f91d33db0effd2a35443f435db6764
smaden <metamaden@users.noreply.github.com>
2018-03-21 10:35:33 -0700

    template for row insert
    Template SQL statement to INSERT new row with data

---
+++ relational_db/insertrow_pubcode_template.sql
@@ -0,0 +1,3 @@
+INSERT INTO public."pubcode_pcidTable"(
+	"CodeTitle", "PCID", "PMID", "PMCID", "PaperPubDate", "CodePubDate", "Authors", "CodeLanguage", "OS", "Dependencies", "Licensing", "Keywords", "Dataset_IDs", "GitHub_URL", "GH_Forks", "GH_OpenIssues", "GH_ClosedIssues", "GH_DateUpdate")
+	VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
