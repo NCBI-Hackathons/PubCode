651dcf4736a86aa7ac27907276e4cf3c955459ca
Sunny <sunny.yu@nih.gov>
2018-03-21 13:16:46 -0400

    update and fix entry-sample.js

--- ui/entry-sample.js
+++ ui/entry-sample.js
@@ -13,7 +13,15 @@ function getEntrySample() {
         id = getParameterByName('id');
     xhr.onreadystatechange = function () {
         if(xhr.readyState === 4 && xhr.status === 200) {
-            var item = JSON.parse(xhr.responseText);
+            var item = JSON.parse(xhr.responseText),
+                keywords = '';
+
+            if(item.Keywords) {
+                for(key in item.Keywords) {
+                    keywords += '<span>' + item.Keywords[key] + '</span>';
+                }
+            }
+
             document.getElementById('title').innerHTML = '<h1>' + item.Title + '</h1>';
             document.getElementById('pmid').innerHTML = item.PubCode_ID || 'N/A';
             document.getElementById('pmcid').innerHTML = item.PubMed_ID || 'N/A';
@@ -26,7 +34,8 @@ function getEntrySample() {
             document.getElementById('dependencies').innerHTML = item.Dependencies || 'N/A';
             document.getElementById('version').innerHTML = item.Version || 'N/A';
             document.getElementById('license').innerHTML = item.License || 'N/A';
-            document.getElementById('github_url').innerHTML = '<a href="' + item.Github_Url + '"><button>Get the code</button></a>'
+            document.getElementById('github_url').innerHTML = '<a href="' + item.Github_Url + '"><button>Get the code</button></a>';
+            document.getElementById('keywords').innerHTML = keywords;
             document.getElementById('description').innerHTML = '<p>' + (item.Description || 'N/A') + '</p>';
             document.getElementById('who_should_use').innerHTML = '<p>' + (item.Who_Should_Use || 'N/A') + '</p>';
             document.getElementById('how_to_use').innerHTML = '<p>' + (item.How_To_Use || 'N/A') + '</p>';
