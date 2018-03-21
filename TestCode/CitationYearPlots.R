8aeec6fe529db91b2f2ea61e1ecf6d6cf4286dc4
Rutter <lindsayannerutter@gmail.com>
2018-03-21 14:57:41 -0400

    Remove year 2018 data as it is not yet complete

---
+++ TestCode/CitationYearPlots.R
@@ -0,0 +1,19 @@
+library(reshape)
+
+dat <- read.csv("../docs/Data/CitationsYear.csv")
+dat <- dat[1:20,]
+# We do not grab 2018 because its data is not yet full
+dat <- dat[,c(1,11:3)]
+colnames(dat) <- c("Software", 2009:2017)
+mdata <- melt(dat, id=c("Software"))
+mdata <- mdata[which(!is.na(mdata$value)),]
+colnames(mdata) <- c("Software", "Year", "Citations")
+
+mdata$Year = as.numeric(as.character(mdata$Year))
+mdata$Citations = as.numeric(as.character(mdata$Citations))
+mdata$Software = as.character(mdata$Software)
+
+# Plot static line plot
+g <- ggplot(data=mdata, aes(x=Year, y=Citations, col=Software)) + geom_line() + theme(legend.position="none")
+# Plot interactive line plot
+ggplotly(g)
